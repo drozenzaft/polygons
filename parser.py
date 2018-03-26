f605813c6e5d977aa5a65935383b20fa8d0c30fb
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-03-26 11:32:03 -0400

    included fix for polygons argument in main

--- parser.py
+++ parser.py
@@ -47,7 +47,7 @@ See the file script for an example of the file format
 """
 ARG_COMMANDS = [ 'line', 'scale', 'move', 'rotate', 'save', 'circle', 'bezier', 'hermite', 'box', 'sphere', 'torus' ]
 
-def parse_file( fname, edges, transform, screen, color ):
+def parse_file( fname, edges, polygons, transform, screen, color ):
 
     f = open(fname)
     lines = f.readlines()
@@ -67,19 +67,19 @@ def parse_file( fname, edges, transform, screen, color ):
 
         if line == 'sphere':
             #print 'SPHERE\t' + str(args)
-            add_sphere(edges,
+            add_sphere(polygons,
                        float(args[0]), float(args[1]), float(args[2]),
                        float(args[3]), step_3d)
 
         elif line == 'torus':
             #print 'TORUS\t' + str(args)
-            add_torus(edges,
+            add_torus(polygons,
                       float(args[0]), float(args[1]), float(args[2]),
                       float(args[3]), float(args[4]), step_3d)
 
         elif line == 'box':
             #print 'BOX\t' + str(args)
-            add_box(edges,
+            add_box(polygons,
                     float(args[0]), float(args[1]), float(args[2]),
                     float(args[3]), float(args[4]), float(args[5]))
 
@@ -129,6 +129,7 @@ def parse_file( fname, edges, transform, screen, color ):
                 
         elif line == 'clear':
             edges = []
+            polygons = []
             
         elif line == 'ident':
             ident(transform)
