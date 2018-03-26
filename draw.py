48690f3f71255c47f0daa9e127310db892328791
Daniel Rozenzaft <drozenzaft@stuy.edu>
2018-03-26 11:36:43 -0400

    started working on draw.py

--- draw.py
+++ draw.py
@@ -3,10 +3,32 @@ from matrix import *
 from math import *
 
 def add_polygon( points, x0, y0, z0, x1, y1, z1, x2, y2, z2 ):
-    pass
+    add_point(points,x0,y0,z0)
+    add_point(points,x1,y1,z1)
+    add_point(points,x2,y2,z2)
 
 def draw_polygons( points, screen, color ):
-    pass
+    if len(matrix) < 3:
+        print 'Need at least 3 points to draw'
+        return
+    point = 0
+    while point < len(matrix) - 2:
+        draw_line( int(matrix[point][0]),
+                   int(matrix[point][1]),
+                   int(matrix[point+1][0]),
+                   int(matrix[point+1][1]),
+                   screen, color)
+        draw_line( int(matrix[point+1][0]),
+                   int(matrix[point+1][1]),
+                   int(matrix[point+2][0]),
+                   int(matrix[point+2][1]),
+                   screen, color)
+        draw_line( int(matrix[point][0]),
+                   int(matrix[point][1]),
+                   int(matrix[point+2][0]),
+                   int(matrix[point+2][1]),
+                   screen, color) 
+        point+= 3
 
 def add_box( points, x, y, z, width, height, depth ):
     x1 = x + width
@@ -14,6 +36,9 @@ def add_box( points, x, y, z, width, height, depth ):
     z1 = z - depth
 
     #front
+    add_polygon(points, x, y, z, x1, y, z, x, y1, z)
+    add_polygon(points, x, y, z, x1, y1, z, x1, y, z)
+    #finish this
     add_edge(points, x, y, z, x1, y, z)
     add_edge(points, x, y1, z, x1, y1, z)
     add_edge(points, x1, y, z, x1, y1, z)
@@ -158,7 +183,7 @@ def add_edge( matrix, x0, y0, z0, x1, y1, z1 ):
     add_point(matrix, x0, y0, z0)
     add_point(matrix, x1, y1, z1)
 
-def add_point( matrix, x, y, z=0 ):
+def add_point( matrix, x, y, z ):
     matrix.append( [x, y, z, 1] )
 
 
